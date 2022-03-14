8750498c089708c4689f6685a6f9f6fb33e55118
Petar Dobrev <peterdobrevxd@gmail.com>
2022-03-14 19:01:48 +0200

    sign up - information to database
    - оправих хиперлинк цвета на "Forgot password?"
    - добавих sign up логиката за добавянето към базата данни

---
+++ ConfigurationWebShopDemo/Controllers/SignUpController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfigurationWebShopDemo.Data;
+using ConfigurationWebShopDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigurationWebShopDemo.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private readonly SignUpDbContext _db;
+
+        public SignUpController(SignUpDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //return RedirectToAction("Create");
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(SignUp obj)
+        {
+            _db.SignUp.Add(obj);
+            _db.SaveChanges();  
+            return View();
+        }
+    }
+}
---
+++ ConfigurationWebShopDemo/Data/SignUpDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfigurationWebShopDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfigurationWebShopDemo.Data
+{
+    public class SignUpDbContext :DbContext
+    {
+        public SignUpDbContext(DbContextOptions<SignUpDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<SignUp> SignUp { get; set; }
+    }
+}
